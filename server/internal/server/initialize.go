f1792bc6a5c9f8ad41c52a3da6844355f15fc17e
Ilya Sokolov <is@incsecurity.kz>
2022-12-15 16:58:51 +0300

    cors

--- server/internal/server/initialize.go
+++ server/internal/server/initialize.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
@@ -9,47 +12,53 @@ import (
 )
 
 func (s *Server) initRoutes() *gin.Engine {
+	//gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 
 	router.Use(
+		cors.Middleware(cors.Config{
+			Origins:        "*",
+			Methods:        "GET, PATCH, POST, DELETE",
+			RequestHeaders: "Origin, Authorization, Content-Type",
+			MaxAge:         50 * time.Second,
+		}),
 		gin.Logger(),
-		CORSMiddleware(),
 		getTokenMiddleware(),
 		s.checkAuthorizationMiddleware(),
 	)
 
 	api := router.Group(`/api`)
 
-	au := api.Group(`/auth`)
-	au.POST(`/sign-in`, s.SignIn)
+	au := api.Group(`auth`)
+	au.POST(`sign-in`, s.SignIn)
 
-	users := api.Group(`/users`)
-	users.GET(`/`, s.GetUsers)
-	users.POST(`/`, s.CreateUser)
-	users.GET(`/:user_id`, s.GetUser)
-	users.PATCH(`/:user_id`, s.UpdateUser)
+	users := api.Group(`users`)
+	users.GET(``, s.GetUsers)
+	users.POST(``, s.CreateUser)
+	users.GET(`:user_id`, s.GetUser)
+	users.PATCH(`:user_id`, s.UpdateUser)
 
-	schedules := api.Group(`/schedules`)
-	schedules.GET(`/`, s.GetSchedules)
-	schedules.PATCH(`/:schedule_id`, s.UpdateSchedule)
-	schedules.POST(`/:schedule_id/confirm`, s.ConfirmSchedule)
-	schedules.POST(`/:schedule_id/unconfirm`, s.UnconfirmSchedule)
+	schedules := api.Group(`schedules`)
+	schedules.GET(``, s.GetSchedules)
+	schedules.PATCH(`:schedule_id`, s.UpdateSchedule)
+	schedules.POST(`:schedule_id/confirm`, s.ConfirmSchedule)
+	schedules.POST(`:schedule_id/unconfirm`, s.UnconfirmSchedule)
 
-	api.GET(`/countries`, s.GetCountries)
-	api.GET(`/cabin-types`, s.GetCabinTypes)
-	api.GET(`/offices`, s.GetOffices)
-	api.GET(`/airports`, s.GetAirports)
+	api.GET(`countries`, s.GetCountries)
+	api.GET(`cabin-types`, s.GetCabinTypes)
+	api.GET(`offices`, s.GetOffices)
+	api.GET(`airports`, s.GetAirports)
 
-	api.POST(`/tickets`, s.AddTicket)
+	api.POST(`tickets`, s.AddTicket)
 
-	reviews := api.Group(`/reviews`)
+	reviews := api.Group(`reviews`)
 
-	reviews.POST(`/`, s.AddReview)
-	reviews.GET(`/brief`, s.GetBriefReviews)
-	reviews.GET(`/detailed`, s.GetDetailedReviews)
+	reviews.POST(``, s.AddReview)
+	reviews.GET(`brief`, s.GetBriefReviews)
+	reviews.GET(`detailed`, s.GetDetailedReviews)
 
 	// Documentation endpoint registration
-	router.GET(`/api/docs/*any`, ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.GET(`api/docs/*any`, ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return router
 }
