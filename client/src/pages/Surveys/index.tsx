af6e4ceb67f2f5bfaec3fe850baf3aa590a45fbe
bouhartsev <m3773517@gmail.com>
2022-12-18 16:05:00 +0300

    Add toggle on survey

--- client/src/pages/Surveys/index.tsx
+++ client/src/pages/Surveys/index.tsx
@@ -1,18 +1,37 @@
-import React, { FC } from 'react';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import TableList from './TableList';
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import TableList from "./TableList";
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
 
-const Surveys: FC = () => {
+const Surveys = () => {
+  const [view, setView] = useState("brief");
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newView: string
+  ) => {
+    if (newView !== null) setView(newView);
+  };
 
   return (
-    <Container maxWidth="xl">
-      <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: 5 }}>
-        Total Results
-      </Typography>
+    <>
+      <Box sx={{ textAlign: "center", mb: 3 }}>
+        <Typography variant="h4">View Surveys</Typography>
+        <ToggleButtonGroup
+          color="primary"
+          value={view}
+          exclusive
+          onChange={handleChange}
+          aria-label="View"
+          sx={{mt:2}}
+        >
+          <ToggleButton value="brief">Results Summary</ToggleButton>
+          <ToggleButton value="detailed">Detailed Results</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
 
       <TableList />
-    </Container>
+    </>
   );
 };
 
